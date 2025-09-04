df2c0f816ee0cc416c6ddb31ba4af9114f62840a
k9ert <117085+k9ert@users.noreply.github.com>
2025-09-04 21:06:12 +0200

    ci: rollback tilt-ci

--- dev/bin/tilt-ci.sh
+++ dev/bin/tilt-ci.sh
@@ -16,15 +16,11 @@ export HONEYCOMB_DATASET=your_dataset_name
 export HOST_PROJECT_PATH=$(pwd)
 export COMPOSE_PROJECT_NAME=blink-quickstart
 export GALOY_GRAPHQL_URI=http://localhost:4455/graphql
-export GALOY_PHONE_CODE=403370
 echo "        HONEYCOMB_API_KEY=${HONEYCOMB_API_KEY}"
 echo "        HONEYCOMB_DATASET=${HONEYCOMB_DATASET}"
 echo "        HOST_PROJECT_PATH=${HOST_PROJECT_PATH}"
 echo "        COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME}"
 echo "        GALOY_GRAPHQL_URI=${GALOY_GRAPHQL_URI}"
-
-echo "        GALOY_PHONE_NUMBER=${GALOY_PHONE_NUMBER}"
-echo "        GALOY_PHONE_CODE=${GALOY_PHONE_CODE}"
 
 # Run tilt ci and capture its output and exit status
 tilt ci --file "${REPO_ROOT}/Tiltfile" \
