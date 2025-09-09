159b0fe1de5ca1b44a521259274396640a934327
k9ert <117085+k9ert@users.noreply.github.com>
2025-09-09 12:48:04 +0200

    fix: flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Stablesats";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -42,9 +42,6 @@
           jq
           tilt
         ]
-        ++ lib.optionals pkgs.stdenv.isDarwin [
-          darwin.apple_sdk.frameworks.SystemConfiguration
-        ];
 
       devEnvVars = rec {
         PGDATABASE = "pg";
