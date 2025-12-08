69fedc72c0d8012847f621f32671dbb9e0d5005b
CI blinkbitcoinbot <202112752+blinkbitcoinbot@users.noreply.github.com>
2025-12-08 21:05:59 +0000

    ci(shared): bump vendored ci files

--- ci/vendor/tasks/docker-bump-image-digest.sh
+++ ci/vendor/tasks/docker-bump-image-digest.sh
@@ -18,10 +18,10 @@ sed -i "s|\(digest: \"${digest}\"\).*$|\1 # METADATA:: repository=https://github
 yq -i e '.appVersion = strenv(app_version)' ./charts/${CHARTS_SUBDIR}/Chart.yaml
 
 if [[ -z $(git config --global user.email) ]]; then
-  git config --global user.email "bot@galoy.io"
+  git config --global user.email "202112752+blinkbitcoinbot@users.noreply.github.com"
 fi
 if [[ -z $(git config --global user.name) ]]; then
-  git config --global user.name "CI Bot"
+  git config --global user.name "CI blinkbitcoinbot"
 fi
 
 (
