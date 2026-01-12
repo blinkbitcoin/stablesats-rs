6522018024f01f2515c1e20194557a50ff0c4ba9
CI blinkbitcoinbot <202112752+blinkbitcoinbot@users.noreply.github.com>
2026-01-12 15:01:36 +0000

    ci(shared): bump vendored ci files

--- ci/vendor/tasks/docker-prep-docker-build-env.sh
+++ ci/vendor/tasks/docker-prep-docker-build-env.sh
@@ -4,8 +4,11 @@
 #! Don't change this file, instead change it in github.com/blinkbitcoin/concourse-shared
 
 if [[ -f version/version ]]; then
-  echo "VERSION=$(cat version/version)" >> repo/.env
+  echo -e "\nVERSION=$(cat version/version)" >> repo/.env
 fi
 
 echo "COMMITHASH=$(cat repo/.git/ref)" >> repo/.env
 echo "BUILDTIME=$(date -u '+%F-%T')" >> repo/.env
+
+echo "    --> repo/.env contents:"
+cat repo/.env
